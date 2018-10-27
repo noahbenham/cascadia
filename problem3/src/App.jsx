11c445f2a119b2949a5d62e60fd7a9cb95817f5c
Noah Benham <noah.benham@cerner.com>
2018-10-26 20:57:56 -0500

    Twitch logo in header

--- problem3/src/App.jsx
+++ problem3/src/App.jsx
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { IntlProvider } from 'react-intl';
 import classNames from 'classnames/bind';
 import ContentContainer from 'terra-content-container';
+import Image from 'terra-image';
 import { messages } from './aggregated-translations/en'; // Import aggregated translations for English
 import GameSearch from './GameSearch';
 import styles from './App.scss';
 
 const cx = classNames.bind(styles);
 
-export default class App extends Component {
-  render() {
-    const header = <div className={cx('header')}>Twitch Game Search</div>;
+export default function App() {
+  const header = (
+    <div className={cx('header')}>
+      <Image src="/glitch_white.svg" height="22" width="22" className={cx('twitch-logo')} />
+      Twitch Game Search
+    </div>
+  );
 
-    return (
-      <IntlProvider locale="en" messages={messages}>
-        <ContentContainer header={header} fill>
-          <GameSearch />
-        </ContentContainer>
-      </IntlProvider>
-    );
-  }
+  return (
+    <IntlProvider locale="en" messages={messages}>
+      <ContentContainer header={header} fill>
+        <GameSearch />
+      </ContentContainer>
+    </IntlProvider>
+  );
 }
