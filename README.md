2d95744edd0a6a2ba2a86f4a88711f035f81a2ef
Noah Benham <noah.benham@cerner.com>
2018-10-27 15:45:56 -0500

    Write README & docs

---
+++ README.md
@@ -0,0 +1,9 @@
+# CascadiaJS
+
+### Problem 1 & 2
+
+Specifications were not given for how input would be provided. Thus, example values were hard-coded.
+
+### Problem 3
+
+Create React App was used. Run `yarn install`, then `yarn start` to view this problem.
--- problem2.js
+++ problem2.js
@@ -1,7 +1,19 @@
+/**
+ * An assmpution was made that the 9999999.99 example should have had an
+ * additional two 9s so the numeric value & text match.
+ * 
+ * Additionally, "and" should only be used between dollars and cents values
+ * so output here will not exactly match exapmle output.
+ */
+
 const single = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const tennums = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
+/**
+ * Parses and returns the textual value of the integer input.
+ * @param {Number} input - Accepts integer values 0 - 999999999
+ */
 function recursiveDollars(input) {
   const millions = Math.floor(input / 1000000);
   let toSubtract = millions * 1000000;
@@ -19,10 +31,10 @@ function recursiveDollars(input) {
 
   let strToReturn = '';
   if (millions) {
-    strToReturn += `${recursiveDollars(millions)} million`;
+    strToReturn += `${recursiveDollars(millions)} million${thousands || hundreds ? ', ' : ''}`;
   }
   if (thousands) {
-    strToReturn += `${recursiveDollars(thousands)} thousand`;
+    strToReturn += `${recursiveDollars(thousands)} thousand${hundreds ? ', ' : ''}`;
   }
   if (hundreds) {
     strToReturn += `${recursiveDollars(hundreds)} hundred`;
@@ -31,16 +43,21 @@ function recursiveDollars(input) {
   if (tens === 1) {
     strToReturn += ` ${teens[ones]} `;
   } else if (tens) {
-    strToReturn += ` ${tennums[tens - 2]} `;
+    strToReturn += ` ${tennums[tens - 2]}${ones ? '-' : ' '}`;
   }
 
   if (ones && tens !== 1) {
-    strToReturn += ` ${single[ones]} `;
+    strToReturn += `${single[ones]} `;
   }
 
   return strToReturn;
 }
 
+/**
+ * Returns the textual value of the numeric input. Includes capitalization, commas & dashes where appropriate.
+ * @throws Error if non-numeric input or input is out of acceptable range.
+ * @param {Number} input - Accepts values 0.00 - 999999999.99
+ */
 function toTextDollars(input) {
   if (typeof input !== 'number') {
     throw new Error('Amount must be a number');
@@ -69,7 +86,7 @@ const inputs = [
   1000,
   1000000,
   1545120,
-  9999999.99,
+  999999999.99,
   245.13,
 ];
 
